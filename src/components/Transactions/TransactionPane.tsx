4b3d8c7737e3e960439b986d979af0cf86dc7931
shahlahassan123 <124316383+shahlahassan123@users.noreply.github.com>
2024-08-01 23:16:50 +0000

    First commit

--- src/components/Transactions/TransactionPane.tsx
+++ src/components/Transactions/TransactionPane.tsx
@@ -1,46 +1,3 @@
-// import { useState } from "react"
-// import { InputCheckbox } from "../InputCheckbox"
-// import { TransactionPaneComponent } from "./types"
-
-// export const TransactionPane: TransactionPaneComponent = ({
-//   transaction,
-//   loading,
-//   setTransactionApproval: consumerSetTransactionApproval,
-// }) => {
-//   const [approved, setApproved] = useState(transaction.approved)
-
-//   const handleApprovalChange = async (newValue: boolean) => {
-//     await consumerSetTransactionApproval({
-//       transactionId: transaction.id,
-//       newValue,
-//     })
-//     setApproved(newValue)
-//   }
-
-//   return (
-//     <div className="RampPane">
-//       <div className="RampPane--content">
-//         <p className="RampText">{transaction.merchant} </p>
-//         <b>{moneyFormatter.format(transaction.amount)}</b>
-//         <p className="RampText--hushed RampText--s">
-//           {transaction.employee.firstName} {transaction.employee.lastName} - {transaction.date}
-//         </p>
-//       </div>
-//       <InputCheckbox
-//         id={transaction.id}
-//         checked={approved}
-//         disabled={loading}
-//         onChange={handleApprovalChange}
-//       />
-//     </div>
-//   )
-// }
-
-// const moneyFormatter = new Intl.NumberFormat("en-US", {
-//   style: "currency",
-//   currency: "USD",
-// })
-
 import { useState } from "react"
 import { InputCheckbox } from "../InputCheckbox"
 import { TransactionPaneComponent } from "./types"
@@ -50,10 +7,9 @@ export const TransactionPane: TransactionPaneComponent = ({
   loading,
   setTransactionApproval: consumerSetTransactionApproval,
 }) => {
-
   const [approved, setApproved] = useState(transaction.approved)
 
-   //Handle approval status change
+  //Handle approval status change
   const handleApprovalChange = async (newValue: boolean) => {
     await consumerSetTransactionApproval({
       transactionId: transaction.id,
